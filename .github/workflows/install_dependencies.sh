59dc0b343344d81f6d4852dc3276b3478321d49f
denpoo1 <den.dyrbalov25@gmail.com>
2024-03-01 16:49:53 +0100

    test send message to telegram

--- .github/workflows/install_dependencies.sh
+++ .github/workflows/install_dependencies.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo apt-get install jq
 sudo apt-get install coreutils
+sudo apt-get install curl
---
+++ .github/workflows/send_message_to_telegram.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+SERVICE_URI="$CLOUD_RUN_SERVICE_URL"
+PROJECT_NAME="Run_App"
+SERVICE_NAME="$GITHUB_REPOSITORY"
+COMMIT="$GITHUB_SHA"
+DEPLOYED_BY="$GITHUB_ACTOR"
+DEPLOYMENT_DATE=$(date +'%Y-%m-%d %H:%M:%S')
+DOCUMENTATION_URI="$CLOUD_RUN_SERVICE_URL/swagger-ui/index.html"
+GITHUB_URI="https://github.com/${GITHUB_REPOSITORY}"
+
+BOT_TOKEN=${{ secrets.BOT_TOKEN }}
+CHAT_ID=${{ secrets.CHAT_ID }}
+
+MESSAGE="<b>✅ Successful Deployment Notification ✅</b>
+Hey Team ✋,
+Exciting news! Another successful deployment has just been completed on the server.
+
+<b>🤖 Project Name:</b> $PROJECT_NAME
+<b>☁️ Service Name:</b> $SERVICE_NAME
+<b>📗 Version:</b> $VERSION
+<b>📪 Commit:</b> $COMMIT
+<b>👨‍💻 Deployed By:</b> $DEPLOYED_BY
+<b>📆 Deployment Date:</b> $DEPLOYMENT_DATE
+<b>📄 Documentation URI:</b> $DOCUMENTATION_URI
+<b>🔍 Service URI:</b> $SERVICE_URI
+<b>☘️ Github URI:</b> $GITHUB_URI
+
+If you have any questions or need further information, don't hesitate to contact us!
+Best regards,
+$DEPLOYED_BY"
+
+# Отправляем сообщение в Telegram с помощью curl
+curl --location "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
+--form "chat_id=$CHAT_ID" \
+--form "text=$MESSAGE" \
+--form "parse_mode=HTML"
